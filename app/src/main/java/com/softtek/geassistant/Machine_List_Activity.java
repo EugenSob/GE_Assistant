11db734a5042c9da8c875ba8ee1acb1068ecd267
Eugenio Soberon <eugenio.soberon@softtek.com>
2017-04-04 19:10:13 -0500

    Creation of Proyect

---
+++ app/src/main/java/com/softtek/geassistant/Machine_List_Activity.java
@@ -0,0 +1,8 @@
+package com.softtek.geassistant;
+
+/**
+ * Created by ricardoa.trevizo on 4/4/2017.
+ */
+
+public class Machine_List_Activity {
+}
---
+++ app/src/main/java/com/softtek/geassistant/QR_Scanner_Activity.java
@@ -0,0 +1,8 @@
+package com.softtek.geassistant;
+
+/**
+ * Created by ricardoa.trevizo on 4/4/2017.
+ */
+
+public class QR_Scanner_Activity {
+}
