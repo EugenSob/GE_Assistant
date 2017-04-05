53e88e8b657b875557acb6848e690c269c692f70
Eugenio Soberon <eugenio.soberon@softtek.com>
2017-04-04 22:17:34 -0500

    Think is ready for sharing

---
+++ app/src/main/java/com/CardAdapter.java
@@ -0,0 +1,56 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.google.android.glass.app.Card;
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+
+public class CardAdapter extends CardScrollAdapter {
+
+	private final List<CardBuilder> mCards;
+
+    public CardAdapter(ArrayList<CardBuilder> cards) {
+        mCards = cards;
+    }
+
+    @Override
+    public int getCount() {
+        return mCards.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mCards.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return mCards.get(position).getView(convertView, parent);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return CardBuilder.getViewTypeCount();
+    }
+
+    @Override
+    public int getItemViewType(int position){
+        return mCards.get(position).getItemViewType();
+    }
+
+    @Override
+    public int getPosition(Object item) {
+        for (int i = 0; i < mCards.size(); i++) {
+            if (getItem(i).equals(item)) {
+                return i;
+            }
+        }
+        return AdapterView.INVALID_POSITION;
+    }
+}
